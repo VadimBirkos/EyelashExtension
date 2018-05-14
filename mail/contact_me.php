f2bf1e9cab2500f71f10cd3e3d52e7c546ac33d4
VadimBirkos <vadim.birkos@gmail.com>
2018-05-15 01:16:52 +0300

    email works. Changed images for info and portfolio

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -15,12 +15,11 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
-// Create the email and send the message
 $to = 'vadim.birkos@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: $email_address\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$email_subject = "Форма обратной связи ресницомания.бел";
+$email_body = "Вы получили новое сообщение.\n\n"."Детали:\n\Имя: $name\n\nEmail: $email_address\n\nКонтактный номер: $phone\n\nСообщение:\n$message";
+$headers = "От: $email_address\n";
+$headers .= "Ответить: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
