cf292c320880aa796d6f024c78ef126bf9858f13
MINSK\vabir <vadim.birkos@gmail.com>
2018-05-16 22:47:51 +0300

    improove perfomance of site.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,12 @@
 <?php
+/*
+For smtp server use: 
+Email - resnicoman@mail.ru
+Pass for email - dflbv3005
+server - smtp.mail.ru
+pass - dflbv3005
+*/
+
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
@@ -15,7 +23,7 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
-$to = 'vadim.birkos@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'resnicoman@mail.ru'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Форма обратной связи ресницомания.бел";
 $email_body = "Вы получили новое сообщение.\n\n"."Детали:\n\Имя: $name\n\nEmail: $email_address\n\nКонтактный номер: $phone\n\nСообщение:\n$message";
 $headers = "От: $email_address\n";
@@ -23,3 +31,4 @@ $headers .= "Ответить: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
+
