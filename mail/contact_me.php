bfe648cd183827769eb6eecccc34cfea3b975285
VadimBirkos <vadim.birkos@gmail.com>
2018-05-19 20:12:10 +0300

    optimazed images and add lazt loading for images on the page.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -25,7 +25,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
    
 $to = 'resnicoman@mail.ru'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Форма обратной связи ресницомания.бел";
-$email_body = "Вы получили новое сообщение.\n\n"."Детали:\n\Имя: $name\n\nEmail: $email_address\n\nКонтактный номер: $phone\n\nСообщение:\n$message";
+$email_body = "Вы получили новое сообщение.\n\n"."Детали:\nИмя: $name\n\nEmail: $email_address\n\nКонтактный номер: $phone\n\nСообщение:\n$message";
 $headers = "От: $email_address\n";
 $headers .= "Ответить: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
